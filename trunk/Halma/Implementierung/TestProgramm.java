258835de79267c4b60991c2eec8b37f7cc403f3c
L1987@hotmail.de <L1987@hotmail.de@e38e3418-2de2-11df-a8e3-ab3d4dd5af44>
2010-03-17 17:04:13 +0000

    Bugs entfernt, nun kompilierbar

--- trunk/Halma/Implementierung/TestProgramm.java
+++ trunk/Halma/Implementierung/TestProgramm.java
@@ -6,12 +6,12 @@ public class TestProgramm{
 		for(int y = 0; y < 17; y++){
 			for(int x = 0; x < 25; x++){
 				if(halmafeld.spielbrett.get(y).get(x).equals(Farbe.KF)){
-					System.out.print("/");
+					System.out.print("-");
 				}else{
 					System.out.print("*");
 				}
-				if(x = 24){
-					System.out.print(/n);
+				if(x == 24){
+					System.out.print("\n");
 				}
 			}
 		}
