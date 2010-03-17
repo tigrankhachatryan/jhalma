42ae430c4014fa1cbf1a52686d0c6ae951a9d496
simschmi@students.uni-mainz.de <simschmi@students.uni-mainz.de@e38e3418-2de2-11df-a8e3-ab3d4dd5af44>
2010-03-17 15:53:09 +0000

    Spielbrett.java editiert: Es sollte nun ein vollständiges Halmafeld erzeugt werden können. Dies gilt noch zu testen!

--- trunk/Halma/Implementierung/Spielbrett.java
+++ trunk/Halma/Implementierung/Spielbrett.java
@@ -1,10 +1,34 @@
+import java.util.ArrayList;
+
 public class Spielbrett{
 	
 	private int anzahlSpieler;
 	private int anzahlZuege;
 	
-	public Spielbrett(){
+	ArrayList<ArrayList<Farbe>> spielbrett;
 	
+	public Spielbrett(){
+		spielbrett = new ArrayList<ArrayList<Farbe>>();
+		for(int y = 0; y < 17; y++){
+			ArrayList<Farbe> aList = new ArrayList<Farbe>();
+			spielbrett.add(y, aList);
+			for(int x = 0; x < 25; x++){
+				if((y%2 = 0 && x%2 = 0) || (y%2 = 1 && x%2 = 1)){
+					spielbrett.get(y).add(x, Farbe.Frei);
+				}else{
+					spielbrett.get(y).add(x, Farbe.KF);
+				}
+				if(((x < (12-y) || x > (12+y)) && y < 4) || ((x < ((24-y) || x > y) && y > 12) ){
+					spielbrett.get(y).add(x, Farbe.KF);
+				}
+				if((x < (y-4) || x > (28-y)) && (y > 4 && y < 9)){
+					spielbrett.get(y).add(x, Farbe.KF);
+				}
+				if((x < (12-y) || x > (y+12) && (y > 8 && y < 12)){
+					spielbrett.get(y).add(x, Farbe.KF);
+				}
+			}
+		}
 	}
 	
 	public int zaehleZuege(String name){
