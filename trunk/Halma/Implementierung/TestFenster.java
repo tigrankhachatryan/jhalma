4da74494fe198779edb779ab515f03253aeaf78f
L1987@hotmail.de <L1987@hotmail.de@e38e3418-2de2-11df-a8e3-ab3d4dd5af44>
2010-03-22 21:31:37 +0000

    package importe und combobox filling angepasst

--- trunk/Halma/Implementierung/TestFenster.java
+++ trunk/Halma/Implementierung/TestFenster.java
@@ -1,3 +1,5 @@
+import view.*;
+import model.*;
 public class TestFenster{
 	public static void main(String[] args){
 		
--- trunk/Halma/Implementierung/view/EinstellungenFenster.java
+++ trunk/Halma/Implementierung/view/EinstellungenFenster.java
@@ -1,5 +1,6 @@
 package view;
 
+import model.*;
 import java.awt.Color;
 import java.awt.GridLayout;
 import javax.swing.JButton;
@@ -15,24 +16,23 @@ public class EinstellungenFenster extends JFrame{
 	JComboBox comboBoxFarbeSpielbrett;
 	
 	public EinstellungenFenster(){
-	
+		Farbe[] arrayFarben = Farbe.values();
+		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		this.setSize(300, 200);
 		
-		this.setLayout(new GridLayout(4, 4, 10, 10));
+		this.setLayout(new GridLayout(4, 1, 10, 10));
 		
 		this.buttonAbbrechen = new JButton("Abbrechen");
 		this.buttonOK = new JButton("OK");
 		this.labelFarbeSpielbrett = new JLabel("Farbe des Spielbretts auswählen");
+		
 		this.comboBoxFarbeSpielbrett = new JComboBox();
-		this.comboBoxFarbeSpielbrett.addItem(Farbe.Blau);
-		this.comboBoxFarbeSpielbrett.addItem(Farbe.Rot);
-		this.comboBoxFarbeSpielbrett.addItem(Farbe.Gelb);
-		this.comboBoxFarbeSpielbrett.addItem(Farbe.Gruen);
-		this.comboBoxFarbeSpielbrett.addItem(Farbe.Schwarz);
-		this.comboBoxFarbeSpielbrett.addItem(Farbe.Weiss);
-		this.comboBoxFarbeSpielbrett.addItem(Farbe.Braun);
+		for(Farbe f : arrayFarben){
+			if(f != Farbe.KF && f != Farbe.Frei)
+				this.comboBoxFarbeSpielbrett.addItem(f);
+		}		
 		
 		this.getContentPane().add(this.labelFarbeSpielbrett);
 		this.getContentPane().add(this.comboBoxFarbeSpielbrett);
