aa00598685363c34b055e01a679d5ac1034e17b6
L1987@hotmail.de <L1987@hotmail.de@e38e3418-2de2-11df-a8e3-ab3d4dd5af44>
2010-03-24 15:25:57 +0000

    Infrastruktur überarbeitet

--- Halma/Implementierung/view/EinstellungenFenster.java
+++ Halma/Implementierung/view/EinstellungenFenster.java
@@ -1,6 +1,7 @@
 package view;
 import model.*;
 import java.awt.*;
+import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,21 +10,35 @@ import javax.swing.JComboBox;
 
 public class EinstellungenFenster extends JFrame{
 	
-	JButton buttonAbbrechen, buttonOK;
-	JLabel labelFarbeSpielbrett;
-	JComboBox comboBoxFarbeSpielbrett;
-	JPanel panelLeer;
-	GridBagConstraints cBAbbrechen, cBOK, cLFarbeSpielbrett, cCBFarbeSpielbrett, cPLeer;
+	private JButton buttonAbbrechen, buttonOK;
+	private JLabel labelFarbeSpielbrett;
+	private JComboBox comboBoxFarbeSpielbrett;
+	private JPanel panelLeer;
+	private GridBagConstraints cBAbbrechen, cBOK, cLFarbeSpielbrett, cCBFarbeSpielbrett, cPLeer;
 	
+	/*
+	 * Standard-Konstruktor.
+	 */
 	public EinstellungenFenster(){
-	
+		// Basiseinstellungen
 		super("Einstellungen");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		//this.setSize(300, 150); wird durch this.pack(); hinfällig
 		this.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 		this.getContentPane().setLayout(new GridBagLayout());
-		
-		this.buttonAbbrechen = new JButton("Abbrechen");
+		// Komponenten hinzufügen
+		this.initializeGridBagContraints();
+		this.addButtons();
+		this.addSelectionView();
+		// Fenstergrösse anpasen und zeigen
+		this.pack();	
+		this.setVisible(true);
+	}
+	
+	
+	/*
+	 * Hilfsmethode
+	 */
+	private void initializeGridBagContraints(){
 		this.cBAbbrechen = new GridBagConstraints();
 		this.cBAbbrechen.gridx = 2;
 		this.cBAbbrechen.gridy = 3;
@@ -32,9 +47,7 @@ public class EinstellungenFenster extends JFrame{
 		this.cBAbbrechen.anchor = GridBagConstraints.PAGE_END;
 		this.cBAbbrechen.fill = GridBagConstraints.HORIZONTAL;
 		this.cBAbbrechen.insets = new Insets(2,50,2,2);
-		this.getContentPane().add(buttonAbbrechen, cBAbbrechen);
-		
-		this.buttonOK = new JButton("OK");
+			
 		this.cBOK = new GridBagConstraints();
 		this.cBOK.gridx = 3;
 		this.cBOK.gridy = 3;
@@ -43,34 +56,21 @@ public class EinstellungenFenster extends JFrame{
 		this.cBOK.anchor = GridBagConstraints.PAGE_END;
 		this.cBOK.fill = GridBagConstraints.HORIZONTAL;
 		this.cBOK.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(buttonOK, cBOK);
 		
-		this.labelFarbeSpielbrett = new JLabel("Farbe des Spielbretts");
 		this.cLFarbeSpielbrett = new GridBagConstraints();
 		this.cLFarbeSpielbrett.gridx = 0;
 		this.cLFarbeSpielbrett.gridy = 0;
 		this.cLFarbeSpielbrett.weightx = 0.5;
 		this.cLFarbeSpielbrett.fill = GridBagConstraints.HORIZONTAL;
 		this.cLFarbeSpielbrett.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelFarbeSpielbrett, cLFarbeSpielbrett);
-		
-		this.comboBoxFarbeSpielbrett = new JComboBox();
+			
 		this.cCBFarbeSpielbrett = new GridBagConstraints();
 		this.cCBFarbeSpielbrett.gridx = 0;
 		this.cCBFarbeSpielbrett.gridy = 1;
 		this.cCBFarbeSpielbrett.weightx = 0.5;
 		this.cCBFarbeSpielbrett.fill = GridBagConstraints.HORIZONTAL;
 		this.cCBFarbeSpielbrett.insets = new Insets(2,2,2,2);
-		
-		Farbe[] arrayFarben = Farbe.values();
-		
-		for(Farbe f : arrayFarben){
-			if(f != Farbe.KF && f != Farbe.Frei)
-				this.comboBoxFarbeSpielbrett.addItem(f);
-		}		
-		this.getContentPane().add(comboBoxFarbeSpielbrett, cCBFarbeSpielbrett);
-		
-		this.panelLeer = new JPanel();
+			
 		this.cPLeer = new GridBagConstraints();
 		this.cPLeer.gridx = 0;
 		this.cPLeer.gridy = 2;
@@ -78,10 +78,34 @@ public class EinstellungenFenster extends JFrame{
 		this.cPLeer.weightx = 0.5;
 		this.cPLeer.fill = GridBagConstraints.HORIZONTAL;
 		this.cPLeer.insets = new Insets(0,0,100,100);
-		this.getContentPane().add(panelLeer, cPLeer);
-		
-		this.pack();
-		
-		this.setVisible(true);
+	}
+	
+	/*
+	 * Hilfsmethode
+	 */
+	private void addButtons(){
+		this.buttonAbbrechen = new JButton("Abbrechen");
+		this.getContentPane().add(buttonAbbrechen, cBAbbrechen);
+		this.buttonOK = new JButton("OK");
+		this.getContentPane().add(buttonOK, cBOK);
+	}
+	
+	/*
+	 * Hilfsmethode
+	 */
+	private void addSelectionView(){
+		this.labelFarbeSpielbrett = new JLabel("Farbe des Spielbretts");
+		this.getContentPane().add(labelFarbeSpielbrett, cLFarbeSpielbrett);
+		this.comboBoxFarbeSpielbrett = new JComboBox();
+		//-------------------------------------------------------
+		Farbe[] arrayFarben = Farbe.values();
+		for(Farbe f : arrayFarben){
+			if(f != Farbe.KF && f != Farbe.Frei)
+				this.comboBoxFarbeSpielbrett.addItem(f);
+		}		
+		//-------------------------------------------------------
+		this.getContentPane().add(comboBoxFarbeSpielbrett, cCBFarbeSpielbrett);
+		this.panelLeer = new JPanel();
+		this.getContentPane().add(panelLeer, cPLeer);	
 	}
 }
