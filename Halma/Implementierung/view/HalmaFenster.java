a6c780e3c1697bdaa49c5d2e3d87988bdabe18d9
simschmi@students.uni-mainz.de <simschmi@students.uni-mainz.de@e38e3418-2de2-11df-a8e3-ab3d4dd5af44>
2010-03-25 19:14:04 +0000

    HalmaFenster.java überarbeitet

--- Halma/Implementierung/view/HalmaFenster.java
+++ Halma/Implementierung/view/HalmaFenster.java
@@ -6,117 +6,171 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class HalmaFenster extends JFrame{
+public class HalmaFenster{
 
+	private JFrame frame;
 	private JButton buttonSpielBeenden, buttonNeuesSpiel, buttonEinstellungen;
 	private JLabel labelReihe, labelAnzahlZuege, labelSpieler, labelZuege;
-	private JMenu menuHorizontal;
+	private JMenu menu1, menu2;
+	private JMenuBar menuBar;
+	private JMenuItem menuItem1, menuItem2, menuItem3, menuItem4, menuItem5, menuItem6;
 	private JPanel panelLeer;
-	private GridBagConstraints cBSpielBeenden, cBNeuesSpiel, cBEinstellungen, cLReihe, cLAnzahlZuege, cLSpieler, cLZuege, cMHorizontal, cPLeer;
+	private GridBagConstraints c;
 	
-	public HalmaFenster{
-		
-		super("Halma");
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		this.getContentPane().setLayout(new GridBagLayout());
-		
+	public HalmaFenster(){
+		// Basiseinstellungen
+		this.frame = new JFrame("Halma");
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		this.frame.getContentPane().setLayout(new GridBagLayout());
+		// Komponenten hinzufügen
+		this.initializeSwingElements();
+		this.setAllocations();
+		// Fenstergröße anpassen und zeigen
+		// this.frame.pack(); wird hier nicht benötigt, da wir die Fenstergröße selbst festlegen wollen.
+		this.frame.setSize(700,600);
+		this.frame.setVisible(true);
+	}
+	
+	/*
+	 * Hilfsmethode
+	 */
+	public void initializeSwingElements(){
+	
 		this.buttonSpielBeenden = new JButton("Spiel beenden");
-		this.cBSpielBeenden = new GridBagConstraints();
-		this.cBSpielBeenden.gridx = 2;
-		this.cBSpielBeenden.gridy = 2;
-		this.cBSpielBeenden.weightx = 0.5;
-		this.cBSpielBeenden.weighty = 1.0;
-		this.cBSpielBeenden.gridheight = 2;
-		this.cBSpielBeenden.anchor = GridBagConstraints.PAGE_END;
-		this.cBSpielBeenden.fill = GridBagConstraints.HORIZONTAL;
-		this.cBSpielBeenden.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(buttonSpielBeenden, cBSpielBeenden);
 		
 		this.buttonNeuesSpiel = new JButton("Neues Spiel");
-		this.cBNeuesSpiel = new GridBagConstraints();
-		this.cBNeuesSpiel.gridx = 3;
-		this.cBNeuesSpiel.gridy = 2;
-		this.cBNeuesSpiel.weightx = 0.5;
-		this.cBNeuesSpiel.weighty = 1.0;
-		this.cBNeuesSpiel.gridheight = 2;
-		this.cBNeuesSpiel.anchor = GridBagConstraints.PAGE_END;
-		this.cBNeuesSpiel.fill = GridBagConstraints.HORIZONTAL;
-		this.cBNeuesSpiel.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(buttonNeuesSpiel, cBNeuesSpiel);
 		
 		this.buttonEinstellungen = new JButton("Einstellungen");
-		this.cBEinstellungen = new GridBagConstraints();
-		this.cBEinstellungen.gridx = 4;
-		this.cBEinstellungen.gridy = 2;
-		this.cBEinstellungen.weightx = 0.5;
-		this.cBEinstellungen.weighty = 1.0;
-		this.cBEinstellungen.gridheight = 2;
-		this.cBEinstellungen.anchor = GridBagConstraints.PAGE_END;
-		this.cBEinstellungen.fill = GridBagConstraints.HORIZONTAL;
-		this.cBEinstellungen.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(buttonEinstellungen, cBEinstellungen);
 		
 		this.labelReihe = new JLabel("Wer ist an der Reihe?");
-		this.cLReihe = new GridBagConstraints();
-		this.cLReihe.gridx = 0;
-		this.cLReihe.gridy = 2;
-		this.cLReihe.weightx = 0.5;
-		this.cLReihe.fill = GridBagConstraints.HORIZONTAL;
-		this.cLReihe.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelReihe, cLReihe);
 		
 		this.labelAnzahlZuege = new JLabel("Anzahl der Züge");
-		this.cLAnzahlZuege = new GridBagConstraints();
-		this.cLAnzahlZuege.gridx = 1;
-		this.cLAnzahlZuege.gridy = 2;
-		this.cLAnzahlZuege.weightx = 0.5;
-		this.cLAnzahlZuege.fill = GridBagConstraints.HORIZONTAL;
-		this.cLAnzahlZuege.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelAnzahlZuege, cLAnzahlZuege);
 		
-		this.labelSpieler = new JLabel("");
-		this.cLSpieler = new GridBagConstraints();
-		this.cLSpieler.gridx = 0;
-		this.cLSpieler.gridy = 3;
-		this.cLSpieler.weightx = 0.5;
-		this.cLSpieler.fill = GridBagConstraints.HORIZONTAL;
-		this.cLSpieler.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelSpieler, cLSpieler);
+		this.labelSpieler = new JLabel("Spieler 2");
 		
-		this.labelZuege = new JLabel("");
-		this.cLZuege = new GridBagConstraints();
-		this.cLZuege.gridx = 1;
-		this.cLZuege.gridy = 3;
-		this.cLZuege.weightx = 0.5;
-		this.cLZuege.fill = GridBagConstraints.HORIZONTAL;
-		this.cLZuege.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelZuege, cLZuege);
-		
-		this.menuHorizontal = new JMenu();
-		this.cMHorizontal = new GridBagConstraints();
-		this.cMHorizontal.gridx = 0;
-		this.cMHorizontal.gridy = 0;
-		this.cMHorizontal.gridwidth = 5;
-		this.cMHorizontal.weightx = 0.5;
-		this.cMHorizontal.fill = GridBagConstraints.HORIZONTAL;
-		this.cMHorizontal.insets = new Insets(2,2,2,2);
+		this.labelZuege = new JLabel("23");
 		
 		this.panelLeer = new JPanel();
-		this.cPLeer = new GridBagConstraints();
-		this.cPLeer.gridx = 0;
-		this.cPLeer.gridy = 1;
-		this.cPLeer.gridwidth = 5;
-		this.cPLeer.gridheight = 4;
-		this.cPLeer.weightx = 0.5;
-		this.cPLeer.fill = GridBagConstraints.HORIZONTAL;
-		this.cPLeer.insets = new Insets(0,0,0,0);
-		this.getContentPane().add(panelLeer, cPLeer);
 		
-		this.pack();
+		this.menuBar = new JMenuBar();
 		
-		this.setVisible(true);
+		this.menu1 = new JMenu("Spiel");
+		
+		this.menu2 = new JMenu("Hilfe");
+		
+		this.menuItem1 = new JMenuItem("Neues Spiel");
+		
+		this.menuItem2 = new JMenuItem("Einstellungen");
+		
+		this.menuItem3 = new JMenuItem("Bestenliste");
+		
+		this.menuItem4 = new JMenuItem("Spiel beenden");
+		
+		this.menuItem5 = new JMenuItem("Spielregeln");
+		
+		this.menuItem6 = new JMenuItem("Info");
 	}
-
+		
+	/*
+	 * Hilfsmethode
+	 */
+	public void setAllocations(){
+	
+		this.c = new GridBagConstraints();
+		// buttonSpielBeenden
+		this.c.gridx = 2;
+		this.c.gridy = 2;
+		this.c.weightx = 0.5;
+		this.c.weighty = 1.0;
+		this.c.gridheight = 2;
+		this.c.anchor = GridBagConstraints.PAGE_END;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(buttonSpielBeenden, c);
+		// buttonNeuesSpiel
+		this.c.gridx = 3;
+		this.c.gridy = 2;
+		this.c.weightx = 0.5;
+		this.c.weighty = 1.0;
+		this.c.gridheight = 2;
+		this.c.anchor = GridBagConstraints.PAGE_END;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(buttonNeuesSpiel, c);
+		// buttonEinstellungen
+		this.c.gridx = 4;
+		this.c.gridy = 2;
+		this.c.weightx = 0.5;
+		this.c.weighty = 1.0;
+		this.c.gridheight = 2;
+		this.c.anchor = GridBagConstraints.PAGE_END;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(buttonEinstellungen, c);
+		// labelReihe // Müsste eine Zelle weiter oben sein.
+		this.c.gridx = 0;
+		this.c.gridy = 2;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelReihe, c);
+		// labelAnzahlZuege // Müsste eine Zelle weiter oben sein.
+		this.c.gridx = 1;
+		this.c.gridy = 2;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelAnzahlZuege, c);
+		// labelSpieler
+		this.c.gridx = 0;
+		this.c.gridy = 3;
+		this.c.weightx = 0.5;
+		this.c.anchor = GridBagConstraints.PAGE_END;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.labelSpieler.setOpaque(true);
+		this.labelSpieler.setBackground(Color.WHITE);
+		this.frame.getContentPane().add(labelSpieler, c);
+		// labelZuege
+		this.c.gridx = 1;
+		this.c.gridy = 3;
+		this.c.weightx = 0.5;
+		this.c.anchor = GridBagConstraints.PAGE_END;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.labelZuege.setOpaque(true);
+		this.labelZuege.setBackground(Color.WHITE);
+		this.frame.getContentPane().add(labelZuege, c);
+		// menuBar
+		this.c.gridx = 0;
+		this.c.gridy = 0;
+		this.c.gridwidth = 5;
+		this.c.weightx = 0.5;
+		this.c.anchor = GridBagConstraints.PAGE_START;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(0,0,0,0);
+		this.menuBar.add(menu1);
+		this.menuBar.add(menu2);
+		this.menu1.add(menuItem1);
+		this.menu1.add(menuItem2);
+		this.menu1.add(menuItem3);
+		this.menu1.add(menuItem4);
+		this.menu2.add(menuItem5);
+		this.menu2.add(menuItem6);
+		this.frame.getContentPane().add(menuBar, c);
+		// panelLeer
+		this.c = new GridBagConstraints();
+		this.c.gridx = 0;
+		this.c.gridy = 1;
+		this.c.gridwidth = 5;
+		this.c.gridheight = 4;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(0,0,0,0);
+		this.frame.getContentPane().add(panelLeer, c);
+	}
 }
