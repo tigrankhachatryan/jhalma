e24909a1bd6f2b8c38998d3f3bd286c49e968c32
simschmi@students.uni-mainz.de <simschmi@students.uni-mainz.de@e38e3418-2de2-11df-a8e3-ab3d4dd5af44>
2010-03-25 17:41:41 +0000

    EinstellungenFenster.java, NeuesSpielFenster.java und SpielBeenden.java strukturiert

--- trunk/Halma/Implementierung/view/NeuesSpielFenster.java
+++ trunk/Halma/Implementierung/view/NeuesSpielFenster.java
@@ -7,231 +7,236 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JComboBox;
 
-public class NeuesSpielFenster extends JFrame{
+public class NeuesSpielFenster{
 
-		private JButton buttonOK, buttonAbbrechen;
-		private JLabel labelAnzahlSpieler, labelFarbeSpieler, labelSpieler1, labelSpieler2, labelSpieler3, labelSpieler4, labelSpieler5, labelSpieler6;
-		private JComboBox comboBoxAnzahlSpieler, comboBoxFarbeSpieler1, comboBoxFarbeSpieler2, comboBoxFarbeSpieler3, comboBoxFarbeSpieler4, comboBoxFarbeSpieler5, comboBoxFarbeSpieler6;
-		private JPanel panelLeer;
-		private GridBagConstraints cBOK, cBAbbrechen, cLAnzahlSpieler, cLFarbeSpieler, cLSpieler1, cLSpieler2, cLSpieler3, cLSpieler4, cLSpieler5, cLSpieler6, cCBAnzahlSpieler,
-									cCBFarbeSpieler1, cCBFarbeSpieler2, cCBFarbeSpieler3, cCBFarbeSpieler4, cCBFarbeSpieler5, cCBFarbeSpieler6, cPLeer;
-		
-		public NeuesSpielFenster(){
-				
-		super("Neues Spiel");
-				
-		Farbe[] arrayFarben = Farbe.values();
-		
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		this.getContentPane().setLayout(new GridBagLayout());
-		
-		this.buttonOK = new JButton("        OK        ");
-		this.cBOK = new GridBagConstraints();
-		this.cBOK.gridx = 3;
-		this.cBOK.gridy = 9;
-		this.cBOK.weightx = 0.5;
-		this.cBOK.weighty = 1.0;
-		this.cBOK.anchor = GridBagConstraints.PAGE_END;
-		this.cBOK.fill = GridBagConstraints.HORIZONTAL;
-		this.cBOK.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(buttonOK, cBOK);
-		
-		this.buttonAbbrechen = new JButton("Abbrechen");
-		this.cBAbbrechen = new GridBagConstraints();
-		this.cBAbbrechen.gridx = 2;
-		this.cBAbbrechen.gridy = 9;
-		this.cBAbbrechen.weightx = 0.5;
-		this.cBAbbrechen.weighty = 1.0;
-		this.cBAbbrechen.anchor = GridBagConstraints.PAGE_END;
-		this.cBAbbrechen.fill = GridBagConstraints.HORIZONTAL;
-		this.cBAbbrechen.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(buttonAbbrechen, cBAbbrechen);
-		
-		this.labelAnzahlSpieler = new JLabel("Wählen Sie die Anzahl der Spieler aus.");
-		this.cLAnzahlSpieler = new GridBagConstraints();
-		this.cLAnzahlSpieler.gridx = 1;
-		this.cLAnzahlSpieler.gridy = 0;
-		this.cLAnzahlSpieler.gridwidth = 2;
-		this.cLAnzahlSpieler.weightx = 0.5;
-		this.cLAnzahlSpieler.fill = GridBagConstraints.HORIZONTAL;
-		this.cLAnzahlSpieler.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelAnzahlSpieler, cLAnzahlSpieler);
-		
-		this.comboBoxAnzahlSpieler = new JComboBox();
-		this.cCBAnzahlSpieler = new GridBagConstraints();
-		this.cCBAnzahlSpieler.gridx = 1;
-		this.cCBAnzahlSpieler.gridy = 1;
-		this.cCBAnzahlSpieler.gridwidth = 2;
-		this.cCBAnzahlSpieler.weightx = 0.5;
-		this.cCBAnzahlSpieler.fill = GridBagConstraints.HORIZONTAL;
-		this.cCBAnzahlSpieler.insets = new Insets(2,2,20,2);
+	private JFrame frame;
+	private JButton buttonOK, buttonAbbrechen;
+	private JLabel labelAnzahlSpieler, labelFarbeSpieler, labelSpieler1, labelSpieler2, labelSpieler3, labelSpieler4, labelSpieler5, labelSpieler6;
+	private JComboBox comboBoxAnzahlSpieler, comboBoxFarbeSpieler1, comboBoxFarbeSpieler2, comboBoxFarbeSpieler3, comboBoxFarbeSpieler4, comboBoxFarbeSpieler5, comboBoxFarbeSpieler6;
+	private JPanel panelLeer;
+	private GridBagConstraints c;
+	
+	/*
+	 * Standard-Konstruktor.
+	 */
+	public NeuesSpielFenster(){
+		// Basiseinstellungen	
+		this.frame = new JFrame("Neues Spiel");
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		this.frame.getContentPane().setLayout(new GridBagLayout());
+		// Komponenten hinzufügen
+		this.initializeSwingElements();
+		this.setAllocations();
+		// Fenstergröße anpassen und zeigen
+		this.frame.pack();
+		this.frame.setVisible(true);
+	}
+	
+	/*
+	 * Hilfsmethode
+	 */
+	public void setAllocations(){
+	
+		this.c = new GridBagConstraints();
+		// buttonOK
+		this.c.gridx = 3;
+		this.c.gridy = 9;
+		this.c.weightx = 0.5;
+		this.c.weighty = 1.0;
+		this.c.anchor = GridBagConstraints.PAGE_END;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(buttonOK, c);
+		// buttonAbbrechen
+		this.c.gridx = 2;
+		this.c.gridy = 9;
+		this.c.weightx = 0.5;
+		this.c.weighty = 1.0;
+		this.c.anchor = GridBagConstraints.PAGE_END;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(buttonAbbrechen, c);
+		// labelAnzahlSpieler
+		this.c.gridx = 1;
+		this.c.gridy = 0;
+		this.c.gridwidth = 2;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelAnzahlSpieler, c);
+		// comboBoxAnzahlSpieler
+		this.c.gridx = 1;
+		this.c.gridy = 1;
+		this.c.gridwidth = 2;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,20,2);
 		this.comboBoxAnzahlSpieler.addItem(new String("2 Spieler"));
 		this.comboBoxAnzahlSpieler.addItem(new String("3 Spieler"));
 		this.comboBoxAnzahlSpieler.addItem(new String("4 Spieler"));
 		this.comboBoxAnzahlSpieler.addItem(new String("6 Spieler"));
-		this.getContentPane().add(comboBoxAnzahlSpieler, cCBAnzahlSpieler);
+		this.frame.getContentPane().add(comboBoxAnzahlSpieler, c);	
+		// labelFarbeSpieler
+		this.c.gridx = 1;
+		this.c.gridy = 2;
+		this.c.gridwidth = 2;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelFarbeSpieler, c);
+		// labelSpieler1
+		this.c.gridx = 1;
+		this.c.gridy = 3;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelSpieler1, c);
+		// labelSpieler 2
+		this.c.gridx = 1;
+		this.c.gridy = 4;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelSpieler2, c);
+		// labelSpieler 3
+		this.c.gridx = 1;
+		this.c.gridy = 5;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelSpieler3, c);
+		// labelSpieler 4
+		this.c.gridx = 1;
+		this.c.gridy = 6;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelSpieler4, c);
+		// labelSpieler 5
+		this.c.gridx = 1;
+		this.c.gridy = 7;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelSpieler5, c);
+		// labelSpieler 6
+		this.c.gridx = 1;
+		this.c.gridy = 8;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,20,2);
+		this.frame.getContentPane().add(labelSpieler6, c);
+		// comboBoxSpieler 1
+		this.c.gridx = 2;
+		this.c.gridy = 3;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.füllen(this.comboBoxFarbeSpieler1);
+		this.frame.getContentPane().add(comboBoxFarbeSpieler1, c);
+		// comboBoxSpieler 2
+		this.c.gridx = 2;
+		this.c.gridy = 4;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.füllen(this.comboBoxFarbeSpieler2);
+		this.frame.getContentPane().add(comboBoxFarbeSpieler2, c);
+		// comboBoxSpieler 3
+		this.c.gridx = 2;
+		this.c.gridy = 5;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.füllen(this.comboBoxFarbeSpieler3);
+		this.frame.getContentPane().add(comboBoxFarbeSpieler3, c);
+		// comboBoxSpieler 4
+		this.c.gridx = 2;
+		this.c.gridy = 6;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.füllen(this.comboBoxFarbeSpieler4);
+		this.frame.getContentPane().add(comboBoxFarbeSpieler4, c);
+		// comboBoxSpieler 5
+		this.c.gridx = 2;
+		this.c.gridy = 7;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.füllen(this.comboBoxFarbeSpieler5);
+		this.frame.getContentPane().add(comboBoxFarbeSpieler5, c);
+		// comboBoxSpieler 6
+		this.c.gridx = 2;
+		this.c.gridy = 8;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,20,2);
+		this.füllen(this.comboBoxFarbeSpieler6);
+		this.frame.getContentPane().add(comboBoxFarbeSpieler6, c);
+		// panelLeer
+		this.c.gridx = 0;
+		this.c.gridy = 0;
+		this.c.gridheight = 10;
+		this.c.weightx = 0.5;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,80);
+		this.frame.getContentPane().add(panelLeer, c);
+	}
+	
+	/*
+	 * Hilfsmethode
+	 */
+	public void initializeSwingElements(){
+	
+		this.buttonOK = new JButton("        OK        "); // Die Leerzeichen sollen die size des Buttons anpassen.
+		
+		this.buttonAbbrechen = new JButton("Abbrechen");
+		
+		this.labelAnzahlSpieler = new JLabel("Wählen Sie die Anzahl der Spieler aus.");
+		
+		this.comboBoxAnzahlSpieler = new JComboBox();	
 		
 		this.labelFarbeSpieler = new JLabel("Wählen Sie die Farbe der Spieler aus.");
-		this.cLFarbeSpieler = new GridBagConstraints();
-		this.cLFarbeSpieler.gridx = 1;
-		this.cLFarbeSpieler.gridy = 2;
-		this.cLFarbeSpieler.gridwidth = 2;
-		this.cLFarbeSpieler.weightx = 0.5;
-		this.cLFarbeSpieler.fill = GridBagConstraints.HORIZONTAL;
-		this.cLFarbeSpieler.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelFarbeSpieler, cLFarbeSpieler);
 		
 		this.labelSpieler1 = new JLabel("Spieler 1");
-		this.cLSpieler1 = new GridBagConstraints();
-		this.cLSpieler1.gridx = 1;
-		this.cLSpieler1.gridy = 3;
-		this.cLSpieler1.weightx = 0.5;
-		this.cLSpieler1.fill = GridBagConstraints.HORIZONTAL;
-		this.cLSpieler1.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelSpieler1, cLSpieler1);
 		
 		this.labelSpieler2 = new JLabel("Spieler 2");
-		this.cLSpieler2 = new GridBagConstraints();
-		this.cLSpieler2.gridx = 1;
-		this.cLSpieler2.gridy = 4;
-		this.cLSpieler2.weightx = 0.5;
-		this.cLSpieler2.fill = GridBagConstraints.HORIZONTAL;
-		this.cLSpieler2.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelSpieler2, cLSpieler2);
 		
 		this.labelSpieler3 = new JLabel("Spieler 3");
-		this.cLSpieler3 = new GridBagConstraints();
-		this.cLSpieler3.gridx = 1;
-		this.cLSpieler3.gridy = 5;
-		this.cLSpieler3.weightx = 0.5;
-		this.cLSpieler3.fill = GridBagConstraints.HORIZONTAL;
-		this.cLSpieler3.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelSpieler3, cLSpieler3);
 		
 		this.labelSpieler4 = new JLabel("Spieler 4");
-		this.cLSpieler4 = new GridBagConstraints();
-		this.cLSpieler4.gridx = 1;
-		this.cLSpieler4.gridy = 6;
-		this.cLSpieler4.weightx = 0.5;
-		this.cLSpieler4.fill = GridBagConstraints.HORIZONTAL;
-		this.cLSpieler4.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelSpieler4, cLSpieler4);
-		
+
 		this.labelSpieler5 = new JLabel("Spieler 5");
-		this.cLSpieler5 = new GridBagConstraints();
-		this.cLSpieler5.gridx = 1;
-		this.cLSpieler5.gridy = 7;
-		this.cLSpieler5.weightx = 0.5;
-		this.cLSpieler5.fill = GridBagConstraints.HORIZONTAL;
-		this.cLSpieler5.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelSpieler5, cLSpieler5);
 		
 		this.labelSpieler6 = new JLabel("Spieler 6");
-		this.cLSpieler6 = new GridBagConstraints();
-		this.cLSpieler6.gridx = 1;
-		this.cLSpieler6.gridy = 8;
-		this.cLSpieler6.weightx = 0.5;
-		this.cLSpieler6.fill = GridBagConstraints.HORIZONTAL;
-		this.cLSpieler6.insets = new Insets(2,2,20,2);
-		this.getContentPane().add(labelSpieler6, cLSpieler6);
 		
 		this.comboBoxFarbeSpieler1 = new JComboBox();
-		this.cCBFarbeSpieler1 = new GridBagConstraints();
-		this.cCBFarbeSpieler1.gridx = 2;
-		this.cCBFarbeSpieler1.gridy = 3;
-		this.cCBFarbeSpieler1.weightx = 0.5;
-		this.cCBFarbeSpieler1.fill = GridBagConstraints.HORIZONTAL;
-		this.cCBFarbeSpieler1.insets = new Insets(2,2,2,2);
-		
-		for(Farbe f : arrayFarben){
-			if(f != Farbe.KF && f != Farbe.Frei)
-				this.comboBoxFarbeSpieler1.addItem(f);
-		}		
-		this.getContentPane().add(comboBoxFarbeSpieler1, cCBFarbeSpieler1);
 		
 		this.comboBoxFarbeSpieler2 = new JComboBox();
-		this.cCBFarbeSpieler2 = new GridBagConstraints();
-		this.cCBFarbeSpieler2.gridx = 2;
-		this.cCBFarbeSpieler2.gridy = 4;
-		this.cCBFarbeSpieler2.weightx = 0.5;
-		this.cCBFarbeSpieler2.fill = GridBagConstraints.HORIZONTAL;
-		this.cCBFarbeSpieler2.insets = new Insets(2,2,2,2);
-		
-		for(Farbe f : arrayFarben){
-			if(f != Farbe.KF && f != Farbe.Frei)
-				this.comboBoxFarbeSpieler2.addItem(f);
-		}		
-		this.getContentPane().add(comboBoxFarbeSpieler2, cCBFarbeSpieler2);
 		
 		this.comboBoxFarbeSpieler3 = new JComboBox();
-		this.cCBFarbeSpieler3 = new GridBagConstraints();
-		this.cCBFarbeSpieler3.gridx = 2;
-		this.cCBFarbeSpieler3.gridy = 5;
-		this.cCBFarbeSpieler3.weightx = 0.5;
-		this.cCBFarbeSpieler3.fill = GridBagConstraints.HORIZONTAL;
-		this.cCBFarbeSpieler3.insets = new Insets(2,2,2,2);
-		
-		for(Farbe f : arrayFarben){
-			if(f != Farbe.KF && f != Farbe.Frei)
-				this.comboBoxFarbeSpieler3.addItem(f);
-		}	
-		this.getContentPane().add(comboBoxFarbeSpieler3, cCBFarbeSpieler3);
 		
 		this.comboBoxFarbeSpieler4 = new JComboBox();
-		this.cCBFarbeSpieler4 = new GridBagConstraints();
-		this.cCBFarbeSpieler4.gridx = 2;
-		this.cCBFarbeSpieler4.gridy = 6;
-		this.cCBFarbeSpieler4.weightx = 0.5;
-		this.cCBFarbeSpieler4.fill = GridBagConstraints.HORIZONTAL;
-		this.cCBFarbeSpieler4.insets = new Insets(2,2,2,2);
-		
-		for(Farbe f : arrayFarben){
-			if(f != Farbe.KF && f != Farbe.Frei)
-				this.comboBoxFarbeSpieler4.addItem(f);
-		}	
-		this.getContentPane().add(comboBoxFarbeSpieler4, cCBFarbeSpieler4);
 		
 		this.comboBoxFarbeSpieler5 = new JComboBox();
-		this.cCBFarbeSpieler5 = new GridBagConstraints();
-		this.cCBFarbeSpieler5.gridx = 2;
-		this.cCBFarbeSpieler5.gridy = 7;
-		this.cCBFarbeSpieler5.weightx = 0.5;
-		this.cCBFarbeSpieler5.fill = GridBagConstraints.HORIZONTAL;
-		this.cCBFarbeSpieler5.insets = new Insets(2,2,2,2);
-		
-		for(Farbe f : arrayFarben){
-			if(f != Farbe.KF && f != Farbe.Frei)
-				this.comboBoxFarbeSpieler5.addItem(f);
-		}	
-		this.getContentPane().add(comboBoxFarbeSpieler5, cCBFarbeSpieler5);
 		
 		this.comboBoxFarbeSpieler6 = new JComboBox();
-		this.cCBFarbeSpieler6 = new GridBagConstraints();
-		this.cCBFarbeSpieler6.gridx = 2;
-		this.cCBFarbeSpieler6.gridy = 8;
-		this.cCBFarbeSpieler6.weightx = 0.5;
-		this.cCBFarbeSpieler6.fill = GridBagConstraints.HORIZONTAL;
-		this.cCBFarbeSpieler6.insets = new Insets(2,2,20,2);
+		
+		this.panelLeer = new JPanel();
+	}
+	
+	/*
+	 * Hilfsmethode
+	 */
+	public void füllen(JComboBox comboBox){
+	
+		Farbe[] arrayFarben = Farbe.values();
 		
 		for(Farbe f : arrayFarben){
 			if(f != Farbe.KF && f != Farbe.Frei)
-				this.comboBoxFarbeSpieler6.addItem(f);
-		}	
-		this.getContentPane().add(comboBoxFarbeSpieler6, cCBFarbeSpieler6);
-		
-		this.panelLeer = new JPanel();
-		this.cPLeer = new GridBagConstraints();
-		this.cPLeer.gridx = 0;
-		this.cPLeer.gridy = 0;
-		this.cPLeer.gridheight = 10;
-		this.cPLeer.weightx = 0.5;
-		this.cPLeer.fill = GridBagConstraints.HORIZONTAL;
-		this.cPLeer.insets = new Insets(2,2,2,80);
-		this.getContentPane().add(panelLeer, cPLeer);
-		
-		this.pack();
-		
-		this.setVisible(true);
-		}
+				comboBox.addItem(f);
+		}		
+	}
 }
--- trunk/Halma/Implementierung/view/SpielBeendenFenster.java
+++ trunk/Halma/Implementierung/view/SpielBeendenFenster.java
@@ -3,67 +3,67 @@ import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
-public class SpielBeendenFenster extends JFrame{
+public class SpielBeendenFenster{
 	
+	private JFrame frame;
 	private JButton buttonJa, buttonNein;
 	private JLabel labelSpielBeenden;
-	private JPanel panelLeer1, panelLeer2;
-	private GridBagConstraints cBJa, cBNein, cLSpielBeenden, cPLeer1, cPLeer2;
+	private GridBagConstraints c;
 	
+	/*
+	 * Standard-Konstruktor.
+	 */
 	public SpielBeendenFenster(){
+		// Basiseinstellungen
+		this.frame = new JFrame("Spiel beenden");
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		this.frame.getContentPane().setLayout(new GridBagLayout());
+		// Komponenten hinzufügen
+		this.initializeSwingElements();
+		this.setAllocations();
+		// Fenstergröße anpassen und zeigen
+		this.frame.pack();	
+		this.frame.setVisible(true);
+	}
 	
-		super("Spiel beenden");
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		this.getContentPane().setLayout(new GridBagLayout());
+	/*
+	 * Hilfsmethode
+	 */
+	 public void initializeSwingElements(){
 		
 		this.buttonJa = new JButton("Ja");
-		this.cBJa = new GridBagConstraints();
-		this.cBJa.gridx = 2;
-		this.cBJa.gridy = 1;
-		this.cBJa.fill = GridBagConstraints.HORIZONTAL;
-		this.cBJa.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(buttonJa, cBJa);
 		
-		this.buttonNein = new JButton("         Nein         ");
-		this.cBNein = new GridBagConstraints();
-		this.cBNein.gridx = 1;
-		this.cBNein.gridy = 1;
-		this.cBNein.fill = GridBagConstraints.HORIZONTAL;
-		this.cBNein.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(buttonNein, cBNein);
+		this.buttonNein = new JButton("         Nein         "); // Die Leerzeichen sollen die size des Buttons anpassen.
 		
 		this.labelSpielBeenden = new JLabel("Möchten Sie das Spiel wirklich beenden?");
-		this.cLSpielBeenden = new GridBagConstraints();
-		this.cLSpielBeenden.gridx = 1;
-		this.cLSpielBeenden.gridy = 0;
-		this.cLSpielBeenden.gridwidth = 2;
-		this.cLSpielBeenden.fill = GridBagConstraints.HORIZONTAL;
-		this.cLSpielBeenden.insets = new Insets(2,2,2,2);
-		this.getContentPane().add(labelSpielBeenden, cLSpielBeenden);
+	 }
+	 
+	/*
+	 * Hilfsmethode
+	 */
+	public void setAllocations(){
 		
-		this.panelLeer1 = new JPanel();
-		this.cPLeer1 = new GridBagConstraints();
-		this.cPLeer1.gridx = 0;
-		this.cPLeer1.gridy = 0;
-		this.cPLeer1.gridheight = 2;
-		this.cPLeer1.fill = GridBagConstraints.HORIZONTAL;
-		this.cPLeer1.insets = new Insets(0,0,0,0);
-		this.getContentPane().add(panelLeer1, cPLeer1);
-		
-		this.panelLeer2 = new JPanel();
-		this.cPLeer2 = new GridBagConstraints();
-		this.cPLeer2.gridx = 3;
-		this.cPLeer2.gridy = 0;
-		this.cPLeer2.gridwidth = 2;
-		this.cPLeer2.fill = GridBagConstraints.HORIZONTAL;
-		this.cPLeer2.insets = new Insets(0,0,0,0);
-		this.getContentPane().add(panelLeer2, cPLeer2);
-		
-		this.pack();
-		
-		this.setVisible(true);
+		this.c = new GridBagConstraints();
+		// buttonJa
+		this.c.gridx = 2;
+		this.c.gridy = 1;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(buttonJa, c);
+		// buttonNein
+		this.c.gridx = 1;
+		this.c.gridy = 1;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(buttonNein, c);
+		// labelSpielBeenden
+		this.c.gridx = 1;
+		this.c.gridy = 0;
+		this.c.gridwidth = 2;
+		this.c.fill = GridBagConstraints.HORIZONTAL;
+		this.c.insets = new Insets(2,2,2,2);
+		this.frame.getContentPane().add(labelSpielBeenden, c);
 	}
 }
