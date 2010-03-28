2e6770c93362c9fcc799e4bc20c8122ed1855fcc
simschmi@students.uni-mainz.de <simschmi@students.uni-mainz.de@e38e3418-2de2-11df-a8e3-ab3d4dd5af44>
2010-03-28 20:57:33 +0000

    JAVA Dateien überarbeitet

--- Halma/Implementierung/controller/NSbuttonOKListener.java
+++ Halma/Implementierung/controller/NSbuttonOKListener.java
@@ -1,18 +1,28 @@
 package controller;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class NSbuttonOKListener extends BasicButtonListener{
+public class NSbuttonOKListener implements ActionListener{
 	
-	private JButton buttonOK;
+	private JFrame frame;
+	private String anzahlSpieler;
+	private Farbe fSpieler1, fSpieler2, fSpieler3, fSpieler4, fSpieler5, fSpieler6;
 	
-	public NSbuttonOKListener(JButton buttonOK){
+	public NSbuttonOKListener(JFrame frame, String str, Farbe f1, Farbe f2, Farbe f3, Farbe f4, Farbe f5, Farbe f6){
 		
-		this.buttonOK = buttonOK;
+		this.frame = frame;
+		this.anzahlSpieler = str;
+		this.fSpieler1 = f1;
+		this.fSpieler2 = f2;
+		this.fSpieler3 = f3;
+		this.fSpieler4 = f4;
+		this.fSpieler5 = f5;
+		this.fSpieler6 = f6;
 	}
 	
-	public void mouseClicked(MouseEvent e){
+	public void actionPerformed(ActionEvent e){
 		
 	}
 }
