84afdc0abbdc17ee28a43309495f790d82632bc7
simschmi@students.uni-mainz.de <simschmi@students.uni-mainz.de@e38e3418-2de2-11df-a8e3-ab3d4dd5af44>
2010-04-09 13:28:01 +0000

    view.SpielBeendenFenster also JOptionPane umgeschrieben

--- Halma/Implementierung/view/SpielBeendenFenster.java
+++ Halma/Implementierung/view/SpielBeendenFenster.java
@@ -1,74 +1,30 @@
-package view;
-
-import controller.*;
-import java.awt.*;
-import javax.swing.JButton;
+import java.awt.Frame;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import java.util.Locale;
  
 public class SpielBeendenFenster{
-	
-	private JDialog frame;
-	private JButton buttonJa, buttonNein;
-	private JLabel labelSpielBeenden;
-	private GridBagConstraints c;
+
+	private final JOptionPane optionPane;
+	private final JDialog dialog;
 	
 	/*
-	 * Standard-Konstruktor.
+	 * Custom-Konstruktor.
 	 */
 	public SpielBeendenFenster(JFrame owner){
-		// Basiseinstellungen
-		this.frame = new JDialog(owner,"Spiel beenden",true);
-		this.frame.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		this.frame.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		this.frame.getContentPane().setLayout(new GridBagLayout());
-		// GUI erzeugen
-		this.createGUI();
-		// Listener hinzufügen
-		this.buttonNein.addActionListener(new AbbrechenButtonListener(this.frame));
-		this.buttonJa.addActionListener(new OKButtonListener(this.frame));
-		// Fenstergröße anpassen und zeigen
-		this.frame.pack();	
-		this.frame.setVisible(true);
-	}
-	 
-	/*
-	 * Hilfsmethode
-	 */
-	private void createGUI(){
-		
-		this.c = new GridBagConstraints();
-		// buttonJa
-		this.buttonJa = new JButton("Ja");
-		this.setConstraints(new Integer(2), new Integer(1), new Double(0.0), new Double(0.0), new Integer(1), new Integer(1), new Integer(GridBagConstraints.CENTER),
-							new Integer(GridBagConstraints.HORIZONTAL), new Integer(2), new Integer(2), new Integer(2), new Integer(2));
-		this.frame.getContentPane().add(this.buttonJa, this.c);
-		// buttonNein
-		this.buttonNein = new JButton("         Nein         "); // Die Leerzeichen sollen die size des Buttons anpassen.
-		this.setConstraints(new Integer(1), new Integer(1), new Double(0.0), new Double(0.0), new Integer(1), new Integer(1), new Integer(GridBagConstraints.CENTER),
-							new Integer(GridBagConstraints.HORIZONTAL), new Integer(2), new Integer(2), new Integer(2), new Integer(2));
-		this.frame.getContentPane().add(this.buttonNein, this.c);
-		// labelSpielBeenden
-		this.labelSpielBeenden = new JLabel("Möchten Sie das Spiel wirklich beenden?");
-		this.setConstraints(new Integer(1), new Integer(0), new Double(0.0), new Double(0.0), new Integer(1), new Integer(2), new Integer(GridBagConstraints.CENTER),
-							new Integer(GridBagConstraints.HORIZONTAL), new Integer(2), new Integer(2), new Integer(2), new Integer(2));
-		this.frame.getContentPane().add(this.labelSpielBeenden, this.c);
-	}
-	
-	/*
-	 * Hilfsmethode
-	 */
-	private void setConstraints(Integer gx, Integer gy, Double wx, Double wy, Integer gheight, Integer gwidth, Integer anker, Integer fuellen, Integer a, Integer b, Integer c, Integer d){
-		
-		this.c.gridx = gx.intValue();
-		this.c.gridy = gy.intValue();
-		this.c.weightx = wx.doubleValue();
-		this.c.weighty = wy.doubleValue();
-		this.c.gridheight = gheight.intValue();
-		this.c.gridwidth = gwidth.intValue();
-		this.c.anchor = anker.intValue();
-		this.c.fill = fuellen.intValue();
-		this.c.insets = new Insets(a.intValue(),b.intValue(),c.intValue(),d.intValue());
+		// Initialisierungen
+		dialog = new JDialog(owner,"Spiel beenden",true);
+		optionPane = new JOptionPane("Möchten Sie das Spiel wirklich beenden?", JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION);
+		// optionPane-Einstellungen
+		JOptionPane.setDefaultLocale(Locale.GERMAN);
+		// optionPane zum dialog hinzufügen
+		dialog.setContentPane(optionPane);
+		// dialog-Einstellungen
+		dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+		dialog.setResizable(false);
+		// dialog packen und anzeigen
+		dialog.pack();
+		dialog.setVisible(true);
 	}
 }
