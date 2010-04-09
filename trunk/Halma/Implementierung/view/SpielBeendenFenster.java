389e9d987f41f9f3a10b52c2b557f8b92227f03c
L1987@hotmail.de <L1987@hotmail.de@e38e3418-2de2-11df-a8e3-ab3d4dd5af44>
2010-04-09 13:45:56 +0000

    Custom Component erstellt für die visualisierung der spielfelder. Wir sind nun auf topCASED umgestiegen, für unsere anforderungen optimal: openSource mit support für UML , FSM und per plugIns ein GUI Builder. topCASED ist eine openSource eclipse Version

--- trunk/Halma/Implementierung/view/SpielBeendenFenster.java
+++ trunk/Halma/Implementierung/view/SpielBeendenFenster.java
@@ -1,30 +1,20 @@
-import java.awt.Frame;
-import javax.swing.JDialog;
+package view;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import java.util.Locale;
  
 public class SpielBeendenFenster{
 
-	private final JOptionPane optionPane;
-	private final JDialog dialog;
-	
 	/*
 	 * Custom-Konstruktor.
 	 */
 	public SpielBeendenFenster(JFrame owner){
-		// Initialisierungen
-		dialog = new JDialog(owner,"Spiel beenden",true);
-		optionPane = new JOptionPane("Möchten Sie das Spiel wirklich beenden?", JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION);
-		// optionPane-Einstellungen
-		JOptionPane.setDefaultLocale(Locale.GERMAN);
-		// optionPane zum dialog hinzufügen
-		dialog.setContentPane(optionPane);
-		// dialog-Einstellungen
-		dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-		dialog.setResizable(false);
-		// dialog packen und anzeigen
-		dialog.pack();
-		dialog.setVisible(true);
+		
+		Object[] options = {"Ja", "Nein"};
+		int n = JOptionPane.showOptionDialog(owner,"Willst du das Spiel wirklich beenden ?", "Spiel Beenden",
+										JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+		if(n==0)
+			System.exit(0);		
 	}
 }
